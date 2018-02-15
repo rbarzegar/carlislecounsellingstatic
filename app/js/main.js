dec9354dfddb1400852ab20e19a8d13e8992a4ac
Roland <barzegarroland@gmail.com>
2018-02-15 10:37:43 +0000

    JS complete

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,13 @@
 // The main file is the top file which will bundle everything else and push to app.js
-
+import SmoothScroll from 'smooth-scroll';
 import OffCanvas from './modules/Off-Canvas';
 
 var offCanvas = new OffCanvas();
 offCanvas.toggle();
+
+var scroll = new SmoothScroll('a[href*="#"]', {
+   // Speed & Easing
+   speed: 750, // Integer. How fast to complete the scroll in milliseconds
+   offset: 0, // Integer or Function returning an integer. How far to offset the scrolling anchor location in pixels
+   easing: "easeOut", // Easing pattern to use
+});

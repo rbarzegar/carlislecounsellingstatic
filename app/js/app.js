788af076e3e65da9ae2027bbdd3a0bf517e3acbd
Roland <barzegarroland@gmail.com>
2018-02-18 19:41:46 +0000

    bio inserted

--- app/js/app.js
+++ app/js/app.js
@@ -192,24 +192,28 @@ var FixedNav = function () {
    }
 
    _createClass(FixedNav, [{
-      key: 'fixedNav',
+      key: "fixedNav",
       value: function fixedNav() {
-         var nav = document.querySelector('.main-navigation');
-         var headerTitle = document.getElementById('headerTitle');
-         var topOfNav = headerTitle.offsetHeight;
+         var nav = document.querySelector(".main-navigation");
+         var headerTitle = document.getElementById("headerTitle");
+         var topOfNav = nav.offsetTop;
 
          function fixtheNav() {
             console.log(window.scrollY, topOfNav);
             if (window.scrollY >= topOfNav) {
-               document.body.style.paddingTop = nav.offsetHeight + 'px';
-               document.body.classList.add('fixed-nav');
+               if (window.innerWidth < 700) {
+                  document.body.style.paddingTop = nav.offsetHeight + "px";
+               }
+               document.body.classList.add("fixed-nav");
             } else {
-               document.body.style.paddingTop = 0;
-               document.body.classList.remove('fixed-nav');
+               document.body.classList.remove("fixed-nav");
+               if (window.innerWidth < 700) {
+                  document.body.style.paddingTop = 0;
+               }
             }
          }
 
-         window.addEventListener('scroll', fixtheNav);
+         window.addEventListener("scroll", fixtheNav);
       }
    }]);
 
--- app/js/modules/Fixed-Nav.js
+++ app/js/modules/Fixed-Nav.js
@@ -1,21 +1,26 @@
 class FixedNav {
    fixedNav() {
-      const nav = document.querySelector('.main-navigation');
-      const headerTitle = document.getElementById('headerTitle');
-      const topOfNav = headerTitle.offsetHeight;
+      const nav = document.querySelector(".main-navigation");
+      const headerTitle = document.getElementById("headerTitle");
+      const topOfNav = nav.offsetTop;
 
       function fixtheNav() {
          console.log(window.scrollY, topOfNav);
          if (window.scrollY >= topOfNav) {
-            document.body.style.paddingTop = nav.offsetHeight + 'px';
-            document.body.classList.add('fixed-nav');
+            if (window.innerWidth < 700) {
+               document.body.style.paddingTop = nav.offsetHeight + "px";
+            }
+            document.body.classList.add("fixed-nav");
+            
          } else {
-            document.body.style.paddingTop = 0;
-            document.body.classList.remove('fixed-nav');
+            document.body.classList.remove("fixed-nav");
+            if (window.innerWidth < 700) {
+               document.body.style.paddingTop = 0;
+            }
          }
       }
 
-      window.addEventListener('scroll', fixtheNav);
+      window.addEventListener("scroll", fixtheNav);
    }
 }
 
